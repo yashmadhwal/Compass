4d9fa529790ae692d006155a271372e960696213
yashmadhwal <yashmadhwal@yahoo.co.in>
2021-07-06 12:12:16 +0300

    added Responsive components

--- js/js.js
+++ js/js.js
@@ -1,8 +1,31 @@
+// Navigation MENU
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
 document.getElementById("buttonId").addEventListener("click", function(){
 document.querySelector('.ResponseRecorded').style.display='block';})
 
 document.getElementById("buttonId").addEventListener("click", function(){
 document.getElementById('ResponseArea').style.display='none';})
+
+
+
 
 // Ajax Pluging for Phone Number
 $(window).load(function()
